d6d64ce0cd55096d8d099d84382a23a67ecc57a7
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-17 08:39:13 +0700

    Layout file

--- src/Survey/index.js
+++ src/Survey/index.js
@@ -1,20 +1,85 @@
+const _ = console.log
 
 export class Survey {
-  constructor(questions){
+  constructor(questions, validates = {}){
     this.questions = questions
+    this.validates = validates
+    this.lastQuestion = {}
   }
 
-  getNextQuestion(){
+  ask(){
     const {questions} = this
+    const remainQuestions = questions.filter(ques => typeof ques.answer === "undefined")
+    const hasQues = remainQuestions.length > 0
+    if(!hasQues) return null
+
+    const firstRemain = remainQuestions[0]
+    this.lastQuestion = firstRemain
+
+    return firstRemain
   }
+
+  capture(answer){
+    let {lastQuestion} = this
+    lastQuestion.answer = answer
+    return this
+  }
+
+  setValidationMethods(newValidates){
+    const {validates: lastValidates} = this
+    const validates = {...lastValidates, ...newValidates}
+    this.validates = validates
+  }
+
+  isValid(){
+    const {lastQuestion} = this
+    const {isValid, answer} = lastQuestion
+
+    const noAns = typeof answer === "undefined"
+    if(noAns) return true
+
+    const isStrMethod = typeof isValid === "string"
+    if(isStrMethod){
+      const name = isValid
+      const {validates} = this
+      const validationMethod = validates[name]
+
+      if(!validationMethod){
+        _(`[isValid] ${name}: No validation method`)
+        return true
+      }
+
+      return validationMethod(answer)
+    }
+
+    const isFuncMethod = typeof isValid === "function"
+    if(isFuncMethod) return isValid(answer)
+
+    _(`[isValid] isValid not a function`)
+    return true
+  }
+
+  // validate(questions){
+  //   const
+  // }
 }
 
 
 const s = {
   questions: [
-    {},
-    {},
-    {},
+    {
+      title: "What's your first name?",
+      isValid: "DATE",
+    },
+    {
+      title: "What type of flower you want to buy?",
+      choices: [
+        {title: "tulips"},
+        {title: "rose"},
+        {title: "orange"},
+      ],
+      isValid: (answer) => true
+    },
   ],
   lastQuestion: {},
   answers: [
