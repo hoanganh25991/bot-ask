5f2c78c61a2b2ca7f14c60d02d5e18ed1919f57d
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-01-18 08:38:32 +0700

    Update repl test

--- src/Survey/__tests__/repl.js
+++ src/Survey/__tests__/repl.js
@@ -11,4 +11,6 @@ import questions3 from "./questions3.json"
 // const survey = new Survey(questions3, validates)
 const survey = new Survey(questions3)
 const tinyRepl = repl.start({ prompt: "(^^): " })
+// survey.askField("Name")
+// survey.askField("Flower")
 tinyRepl.context.survey = survey
